8cfd8de4ddba4bdcd4af6ee0b129dc092af33790
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-05-13 21:23:19 +0900

    git test

---
+++ src/test/java/cloud/ieum/IeumApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.ieum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
