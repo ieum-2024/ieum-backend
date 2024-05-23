7c7d90e9263336029a143aee5ced52d1515b7b4a
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-05-23 19:21:22 +0900

    관심사 기능 구현

--- src/main/java/cloud/ieum/interest/Interest.java
+++ src/main/java/cloud/ieum/interest/Interest.java
@@ -1,8 +1,22 @@
 package cloud.ieum.interest;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "interest_tb")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
 public class Interest {
-    int socialId;
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "userId")
+    private Long userId;
+    @Column(name = "categoryId")
+    private Long categoryId;
 
 }
