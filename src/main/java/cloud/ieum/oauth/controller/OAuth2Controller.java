718ef6075135e40bc6685885ee904ef4ae5f802b
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-05-25 11:11:08 +0900

    .

--- src/main/java/cloud/ieum/oauth/controller/OAuth2Controller.java
+++ src/main/java/cloud/ieum/oauth/controller/OAuth2Controller.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
-b
+
 @RestController
 @Slf4j
 @RequiredArgsConstructor
