faa70f829940d8394df1f09b17d09747a8e472ab
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-05-25 11:23:29 +0900

    .

--- src/main/java/cloud/ieum/content/category/CategoryJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package cloud.ieum.content.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategoryJpaRepository extends JpaRepository<Category, Integer> {
-}
